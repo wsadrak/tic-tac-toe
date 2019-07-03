58926750a0b83c647ce739d6b2c85f905298e6ee
wsadrak <w.sadrak@gmail.com>
2019-07-03 14:17:55 +0200

    refactor - removed descriptions

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -1,16 +1,6 @@
 package model;
 
 public enum GameState {
-	IN_PROGRES("in progr"), WIN("You win!"), LOSE("You lose!"), DRAW("It's a draw");
+	IN_PROGRES(), WIN(), LOSE(), DRAW();
 	
-	String desc;
-
-	private GameState(String desc) {
-		this.desc = desc;
-	}
-	
-	@Override
-	public String toString() {
-		return desc;
-	}
 }

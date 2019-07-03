5ea34e1e63f9089f6427074c1f597e07db7f1121
wsadrak <w.sadrak@gmail.com>
2019-07-03 14:18:41 +0200

    created new class for printing data in console

---
+++ src/io/ConsolePrinter.java
@@ -0,0 +1,12 @@
+package io;
+
+public class ConsolePrinter {
+	
+	public void printNextLine(String message) {
+		System.out.println(message);
+	}
+	public void printLine(String message) {
+		System.out.print(message);
+	}
+	
+}
